0ff7988f1eb39f62908e6449e662084954f1cc86
hejia <1464946535@qq.com>
2017-02-18 17:54:42 +0800

    up

---
+++ native/xpy/test/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iostream>
+#include<boost/filesystem.hpp>
+#include "xpy.h"
+
+namespace fs = boost::filesystem;
+using namespace std;
+
+const char* char_array_test1()
+{
+    string t1 = "wert";
+    const size_t len = t1.length()+1;
+    const char* sd = t1.c_str();
+    return sd;
+}
+
+void char_array_test()
+{
+    char * data = new char[128];
+    string s = "I come back!";
+    strcpy(data, s.c_str());
+    cout << data << endl;
+
+    cout << char_array_test1() << endl;
+}
+
+void python_embedding_test()
+{
+	cout << native_get_current_path() << endl;
+    char *program = "python36_xpy";
+     python_start(program, "G:/GameDev/unity/xpy/native/xpy/external/Python-3.6.0");
+    std::cout << python_check_interpreter(program) << "\n";
+    python_finalize();
+}
+
+int main(int argc, char *argv[])
+{
+	python_embedding_test();
+    return 0;
+}
--- native/xpy/xpy/xpy.cpp
+++ native/xpy/xpy/xpy.cpp
@@ -1,14 +1,21 @@
 #include "xpy.h"
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <iostream>
+#include <Python.h>
 #include <boost/filesystem.hpp>
+
+//#define  BOOST_DISABLE_ASSERTS
+#include <boost/assert.hpp>
 
 namespace fs = boost::filesystem;
 using namespace std;
 
+static wchar_t* py_program_name = nullptr;
+static wchar_t* py_home_path = nullptr;
 
-char* native_get_current_path()
+const char* native_get_current_path()
 {
 	fs::path sCurrPath = boost::filesystem::current_path();
 	size_t iBufferSize = sCurrPath.string().length() + 2;
@@ -21,7 +28,7 @@ char* native_get_current_path()
 	return pBuffer;
 }
 
-void  native_release_memory(void *pBuffer)
+void native_release_memory(void *pBuffer)
 {
 	if (nullptr != pBuffer)
 	{
@@ -30,3 +37,53 @@ void  native_release_memory(void *pBuffer)
 	}
 }
 
+void python_start(const char* program, const char* home)
+{
+	BOOST_ASSERT(program != nullptr && home != nullptr);
+	size_t len, converted;
+
+	len = strlen(program) + 1;
+	converted = 0;
+	if (py_program_name != nullptr)
+	{
+		delete[] py_program_name;
+		py_program_name = nullptr;
+	}
+	py_program_name = new wchar_t[len];
+	mbstowcs_s(&converted, py_program_name, len, program, _TRUNCATE);
+	Py_SetProgramName(py_program_name); /* optional but recommended */
+
+	len = strlen(home) + 1;
+	converted = 0;
+	if (py_home_path != nullptr)
+	{
+		delete[] py_home_path;
+		py_home_path = nullptr;
+	}
+	py_home_path = new wchar_t[len];
+	mbstowcs_s(&converted, py_home_path, len, home, _TRUNCATE);
+	Py_SetPythonHome(py_home_path);
+
+	Py_Initialize();
+	PyRun_SimpleString("from time import time,ctime\n"
+		"print('Today is', ctime(time()))\n");
+}
+
+bool python_check_interpreter(const char *program)
+{
+	wstringstream wss1;
+	wss1 << Py_GetProgramName();
+	wstringstream wss2;
+	wss2 << program;
+	bool isEmbedded = wss1.str() == wss2.str();
+	return isEmbedded;
+}
+
+bool python_finalize()
+{
+	if (Py_FinalizeEx() < 0)
+	{
+		return false;
+	}
+	return true;
+}
