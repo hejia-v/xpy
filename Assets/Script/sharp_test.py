2086ff10dae61ee5a6704abda96c8da6512df116
hejia <1464946535@qq.com>
2017-03-05 11:35:59 +0800

    refactor

---
+++ Assets/Script/sharp_test.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+import xpy
+
+g_csfunc = None
+
+def test_1(*arg):
+    print(arg)
+    return test_2
+
+
+def test_2(a):
+    print('test_2 [a: %s]' % a)
+    return a
+
+
+def test_3(*arg):
+    print(arg)
+    return test_4
+
+
+def test_4(a, b):
+    print('test_4 [a: %s], [b: %s]' % (a, b))
+    return a, b
+
+
+def init(c):
+    global g_csfunc
+    print(c)
+    g_csfunc = c
+
+
+def callback(*arg):
+    ret = xpy.csharpcall(g_csfunc, *arg)
+    print("Return:" , ret)
+
+
+def main():
+    # callback(74, 'ag', (2, 'pt'), '8')
+    callback(None)
+    callback(test_1)
+    callback(None, True, False, 124, 0xffffffff + 1234, 34.5, 'hello 哈哈')
+    return True
--- Native/xpy/sharp/Program.cs
+++ Native/xpy/sharp/Program.cs
@@ -9,11 +9,58 @@ namespace sharp
 {
     class Program
     {
+        class MyClass
+        {
+
+        }
+
+        // Need code gen
+        public static string FuncCallByPython(int n, PyEnv.var[] argv)
+        {
+            logger.info("I'm in Python :");
+            for (int i = 1; i < n; i++)
+            {
+                logger.info(string.Format("Args {0} type {1}", i, argv[i].type));
+            }
+
+            // return string
+            // argv[0].type = PyDLL.var_type.STRING;
+            // return "ok";
+
+            argv[0].type = PyEnv.var_type.INTEGER;
+            argv[0].d = 123;
+            return null;
+        }
+
         static void Main(string[] args)
         {
             // TODO: memory leak test
             PyEnv pyEnv = new PyEnv();
             pyEnv.Init();
+
+            // test
+            PyEnv.PyObject test_1 = pyEnv.GetFunction("sharp_test", "test_1");
+            PyEnv.PyObject test_2 = (PyEnv.PyObject)pyEnv.CallFunction(test_1, "aaabbb")[0];
+            MyClass myobj = new MyClass();
+            object[] result1 = pyEnv.CallFunction(test_2, myobj);
+
+            PyEnv.PyObject test_4 = pyEnv.GetFunction("sharp_test", "test_4");
+            object[] result2 = pyEnv.CallFunction(test_4, "Hello World 1", test_1);
+            object[] result3 = pyEnv.CallFunction(test_4, "Hello World 2", 547);
+            object[] result4 = pyEnv.CallFunction(test_4, "Hello World 3", myobj);
+            logger.info((string)result4[0]);
+            logger.info(result4[1].GetType().ToString());
+
+            PyEnv.PyObject gc = pyEnv.GetFunction("gc", "collect");
+            pyEnv.CallFunction(gc);  // gc.collect(generation=2), With no arguments, run a full collection.
+            pyEnv.CollectGarbage();
+
+            PyEnv.PyObject init = pyEnv.GetFunction("sharp_test", "init");
+            PyEnv.SharpFunction func = FuncCallByPython;
+            pyEnv.CallFunction(init, func);
+            PyEnv.PyObject callback = pyEnv.GetFunction("sharp_test", "callback");
+            pyEnv.CallFunction(callback, 1, null, "string");
+
             pyEnv.Destroy();
         }
     }
